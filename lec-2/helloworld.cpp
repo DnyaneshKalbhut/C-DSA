f742ef7d21d6033401017eea391f5c916fea0d3f
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-04-09 23:31:10 +0530

    Lecture 2 completed

---
+++ lec-2/helloworld.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+//    int a = 5;
+//    cout<< a <<endl ;
+//     char ch = 'b';
+//     cout<< ch <<endl;
+//     bool b =true;
+//     cout<<b<<endl;
+//     float f = 1.2;
+//     cout<<f<<endl;
+//    double d= 1.23;
+//    cout<<d<<endl;
+
+//    int size = sizeof(a);
+//    cout<<"size of a is :" << size <<endl;
+
+   int a = 'a';
+   cout <<a<<endl;
+
+    char ch = 98;
+    cout<< ch <<endl;
+
+    char ch1 = 1234;
+    cout<<ch1<<endl;
+
+    unsigned int ab = -123;
+    cout<<ab<<endl;
+
+
+    float f = 2.0/5;
+    cout<<f<<endl;
+    
+
+    cout<< "Namaste Dunia" <<endl;
+}
