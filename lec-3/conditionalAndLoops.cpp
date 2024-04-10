70cb19b97b036f50f7a8903413cd351c3d3fe48e
DnyaneshKalbhut <kalbhutdnyanesh@gmail.com>
2024-04-11 00:31:54 +0530

    condtional loops started

--- lec-3/conditionalAndLoops.cpp
+++ lec-3/conditionalAndLoops.cpp
@@ -5,5 +5,19 @@ int main(){
     int n ;
     cin>>n;
 
-    cout<<"value of n is : "<<n<<endl;
+    // if(n>0){
+    //     cout<< "A is positive" <<endl;
+    // }else{
+    //     cout<<"A is neagative"<<endl;
+    // }
+    int i = 2;
+    while(i<n){
+     if(n%i==0){
+        cout<<"is not a prime number"<<endl;
+     }else{
+        cout << "is prime " <<endl;
+     }
+     i++;
+    }
+
 }
